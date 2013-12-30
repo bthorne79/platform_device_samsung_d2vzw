92edf6717b985ea033ea04a35e05ddca88fe7f07
treken <chupekevin@gmail.com>
2013-12-30 08:50:07 -0600

    Update for OctOS

--- cm.mk
+++ cm.mk
@@ -1,11 +1,5 @@
 $(call inherit-product, device/samsung/d2vzw/full_d2vzw.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2vzw \
     TARGET_DEVICE=d2vzw \
@@ -14,6 +8,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
-PRODUCT_NAME := cm_d2vzw
+PRODUCT_NAME := oct_d2vzw
 PRODUCT_DEVICE := d2vzw
 
