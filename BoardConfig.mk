3d3e43934093f3950d18e2b17df49085a21ee5f4
Carl Windhausen <cjwindhausen@gmail.com>
2014-01-22 11:36:03 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2009 The CyanogenMod Project
+# Copyright (C) 2014 Team-OctOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,4 +30,4 @@
 TARGET_OTA_ASSERT_DEVICE := d2vzw
 
 # Kernel
-TARGET_KERNEL_CONFIG        := cyanogen_d2vzw_defconfig
+TARGET_KERNEL_CONFIG        := d2_vzw_defconfig
